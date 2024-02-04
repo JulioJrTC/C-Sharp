5c914cebf40a07b955948cbb1d944f179695f70a
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-04 17:00:56 -0300

    Treinamento C# - Testes de Codigos

---
+++ ExerciciosC#/TreinamentoCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿//Treinamento C#
+
+//Tipos de variaveis
+
+//Variavel String
+string nomePessoa = "Alexa";
+
+//Variavel Integer
+int numeroInt = 69;
+
+//Variavel Double
+double numeroDouble = 6996;
+
+//Variavel Float
+float numeroFloat = 9669f;
+
+//Variavel Boolean
+bool alexaPeidou = true;
