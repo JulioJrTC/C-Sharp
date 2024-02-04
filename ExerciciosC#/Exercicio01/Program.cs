7602d8919e44fba5bebda54987488153cb52d62e
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-04 16:55:05 -0300

    Exercicio 01

---
+++ ExerciciosC#/Exercicio01/Program.cs
@@ -0,0 +1,8 @@
+﻿//Desafio 01: Crie um programa que escreva "Olá, Mundo!" na tela.
+
+//Solicitando um nome ao usuario
+Console.Write("Digite seu nome: ");
+String nomePessoa=Console.ReadLine();
+
+//Imprimindo a resposta
+Console.WriteLine("Olá " + nomePessoa + "!");
