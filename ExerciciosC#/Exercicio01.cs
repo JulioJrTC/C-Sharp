d51497c12ffbb1984b157f0572a1fb905142b829
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-04 16:38:32 -0300

    Exercicio 01

---
+++ ExerciciosC#/Exercicio01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Desafio 01: Crie um programa que escreva "Olá, Mundo!" na tela.
+
+namespace ExerciciosCSharp
+{
+    internal class Exercicio01
+    {
+        public static void Main(String[] args)
+        {
+            //Declarando uma variavel String
+            String nomePessoa = "Alexa Bliss";
+
+            //Imprimindo uma frase junto a uma variavel
+            Console.WriteLine("Hello " + nomePessoa + "!");
+        }
+    }
+}
---
+++ ExerciciosC#/TreinamentoCSharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosCSharp
+{
+    internal class TreinamentoCSharp
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+
+
+    }
+}
