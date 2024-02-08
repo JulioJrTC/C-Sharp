a5e10e625ca5bd0052835a6ebb6a383ecb49ad8a
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-08 19:36:56 -0300

    Exercicio 03

---
+++ ExerciciosC#/Exercicio03/Program.cs
@@ -0,0 +1,60 @@
+﻿//Desafio 03: Crie um programa que leia dois numeros e mostre a soma entre eles.
+
+//Greetings!
+Console.WriteLine("Bem vindo ao somador da Prof(a) Alexa!");
+
+//Programa principal
+while (true)
+{
+    //Solicitando um valor numerico que será armazenando em uma variavel string
+    Console.Write("Digite o primeiro numero: ");
+    String input1 = Console.ReadLine();
+
+    //Se não for possivel converter a variavel String para Int iremos exibir um erro (e se for, iremos armazenar o valor convertido em uma variavel Int)
+    if (!int.TryParse(input1, out int n1))
+    {
+        Console.WriteLine("Erro! Digite um valor numerico valido!");
+        return;
+    }
+
+    //Solicitando um valor numerico que será armazenando em uma variavel string
+    Console.Write("Digite o segundo numero: ");
+    String input2 = Console.ReadLine();
+
+    //Se não for possivel converter a variavel String para Int iremos exibir um erro (e se for, iremos armazenar o valor convertido em uma variavel Int)
+    if (!int.TryParse(input2, out int n2))
+    {
+        Console.WriteLine("Erro! Digite um valor numerico valido!");
+        return;
+    }
+
+    //Variavel que irá conter a soma dos valores inseridos nas variaveis anteriores
+    int soma = (n1 + n2);
+
+    //Imprimindo resultado
+    Console.WriteLine($"{n1} + {n2} = {soma}");
+
+    //Variavel para o sistema de escolhas
+    string c;
+
+    //Tratamento de erro
+    while (true)
+    {
+        Console.Write("Deseja continuar? [S/N]");
+        c = Console.ReadLine().ToUpper();
+
+        if (c=="N" || c=="S"){
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Erro! Digite S para continuar ou N para sair do programa!");
+        }
+    }
+    
+    if (c=="N")
+    {
+        Console.WriteLine("Obrigado e volte sempre!");
+        break;
+    }
+}
--- ExerciciosC#/TreinamentoCSharp/Program.cs
+++ ExerciciosC#/TreinamentoCSharp/Program.cs
@@ -1,13 +1,1 @@
 ﻿//Treinamento C#
-
-//Solicitando um valor String ao usuario
-using System.Text.RegularExpressions;
-
-Console.Write("Digite um valor (String): ");
-String valorDigitado = Console.ReadLine();
-
-
-if (!Regex.IsMatch(valorDigitado, "^[A-Za-z]+$"))
-{
-    Console.WriteLine("Erro! Vc digitou algum valor alem de Strings!");    
-}
