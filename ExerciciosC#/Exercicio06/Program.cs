f93f0cd25a8fc2efcab7a036f3e81a7374bb26a7
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-15 19:48:08 -0300

    Exercicio 06

--- ExerciciosC#/Exercicio06/Program.cs
+++ ExerciciosC#/Exercicio06/Program.cs
@@ -1,12 +1,28 @@
 ﻿//Desafio 06: Crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada.
 
-Console.Write("Digite um valor numerico: ");
-int num = Convert.ToInt32(Console.ReadLine());
+//Declarando variaveis int e double
+int num, numDobro, numTriplo;
+double numRaizQuadrada;
 
-int numDobro = num * 2;
-int numTriplo = num * 3;
-double numRaizQuadrada = Math.Sqrt(num);
+//Greetings!
+Console.WriteLine("Bem vindo ao calcular de numeros da Prof(a) Alexa!");
 
-Console.WriteLine($"O numero {num} tem como dobro o numero {numDobro}, triplo {numTriplo} e Raiz Quadrada ~{numRaizQuadrada}.");
+//Tratamento de erro
+while (true)
+{
+    try
+    {
+        //Solicitando um valor numerico (e convertendo a variavel para Int)
+        Console.Write("Digite um valor numerico: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }catch(FormatException ex) {Console.WriteLine("Erro! Digite somente valores numericos!");}
+}
 
+//Variaveis matematicas para o calculo do dobro, triplo e raiz quadrada
+numDobro = num * 2;
+numTriplo = num * 3;
+numRaizQuadrada = Math.Sqrt(num);
 
+//Imprimindo o resultado
+Console.WriteLine($"O numero {num} tem como dobro o numero {numDobro}, triplo {numTriplo} e Raiz Quadrada {numRaizQuadrada}.");
--- ExerciciosC#/Exercicio06/obj/Debug/net8.0/Exercicio06.AssemblyInfo.cs
+++ ExerciciosC#/Exercicio06/obj/Debug/net8.0/Exercicio06.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+164ab13406638278d5d64b2a3345f3873fa7d243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e2c95fd606889aaf96e68892dee3568bc2d334f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercicio06")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
