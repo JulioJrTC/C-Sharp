1776cac0ccfef6c08d060fe949384bcd134cace9
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-02-19 16:55:52 -0300

    Exercicio 09

---
+++ ExerciciosC#/Exercicio08/Program.cs
@@ -0,0 +1,16 @@
+﻿//Desafio 08: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+double numM,numCM,numMM;
+
+//Solicitando um valor numerico (double) em Metros.
+Console.Write("Digite um valor numerico em Metros: (m)");
+numM = Convert.ToDouble(Console.ReadLine());
+
+//Convertendo o valor em Metros digitado acima para Centimetros.
+numCM = numM * 100;
+
+//Convertendo o valor em Metros digitado acima para Milimetros.
+numMM = numM * 1000;
+
+//Imprimindo o resultado.
+Console.WriteLine($"{numM}(m) equivale a {numCM}(cm) e {numMM}(mm).");
---
+++ ExerciciosC#/Exercicio09/Program.cs
@@ -0,0 +1,23 @@
+﻿//Desafio 09: Faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+
+int num;
+
+//Tratamento de erro.
+while (true)
+{
+    try
+    {
+        //Solicitando um valor numerico para a tabuada.
+        Console.Write("Digite um valor numerico para a tabuada: ");
+        num = Convert.ToInt32(Console.ReadLine());
+        break;
+    }
+    catch(FormatException ex)
+    {
+        Console.WriteLine("\nErro! Digite somente valores numericos! (Exemplo 69)\n");        
+    }    
+}
+
+//Imprimindo a tabuada.
+Console.WriteLine($"\nTabuada do {num}:");
+for(int n = 1; n < 11; n++) {Console.WriteLine($"{num}x{n} = {num * n}");}
