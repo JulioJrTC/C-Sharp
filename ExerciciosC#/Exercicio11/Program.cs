57ed9061d16a01ca8a74120d693d544b0bc30190
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-03-05 18:22:57 -0300

    Exercicio 12

---
+++ ExerciciosC#/Exercicio11/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Desafio 11: Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área
+e a quantidade de tinta necessaria para pinta-la, sabendo que cada litro de tinta, pinta uma area de 2m². */
+
+Console.Write("Digite um valor numerico para a largura: ");
+double largura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite um valor numerico para a altura: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+double area = largura * altura;
+
+double quantTinta = area/2;
+
+Console.WriteLine($"Será necessario {quantTinta:N2}l de tinta para pintar uma area de {area:N2}m².");
---
+++ ExerciciosC#/Exercicio12/Program.cs
@@ -0,0 +1,42 @@
+﻿// Desafio 12: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+//Biblioteca referente ao "Regex" (tratamento de erro)
+using System.Text.RegularExpressions;
+
+//Greetings!
+Console.WriteLine("Bem vindo ao descontador de preço da Alexa!");
+
+//Declarando uma variavel String que iremos usar para tratarmos de possiveis erros
+string preco;
+
+while (true)
+{
+    //Solicitando um valor monetario ao usuario
+    Console.Write("Insira um valor monetario (Exemplo: 9,99): ");
+    preco = Console.ReadLine();
+
+    //Se o usuario digitar qualquer valor alem de valores numericos, iremos exibir um erro
+    if(Regex.IsMatch(preco, "^[A-Za-z]+$"))
+    {
+        Console.WriteLine("Erro! Digite somente valores numericos!");
+    }
+    else {break;}
+}
+
+//Se o usuario digitar um ponto em vez de virgula com ponto separador, iremos substituir esse ponto por uma virgula
+if (preco.Contains("."))
+{
+    preco = preco.Replace(".", ",");
+}
+
+//Declarando uma nova variavel que irá receber o valor digitado e tratato acima convertido para double
+double precoOriginal = Convert.ToDouble(preco);
+
+//Desconto
+int desconto = 5;
+
+//Formula matematica para o calculo do novo preço
+double novoPreco = precoOriginal - (precoOriginal * desconto / 100);
+
+//Imprimindo resultado final
+Console.WriteLine($"O produto que antes custava R${precoOriginal:N2}, com {desconto}% de desconto irá custar R${novoPreco:N2}");
