f3e605797de78ed35165e63cc58b8af04e4b1661
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-03-07 18:08:34 -0300

    Exercicio 14

---
+++ ExerciciosC#/Exercicio13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Desafio 13: Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento.
+
+//Solicitando o nome do funcionario
+Console.Write("Digite o nome do funcionario: ");
+string nomeFuncionario = Console.ReadLine();
+
+//Solicitando o salario atual do funcionario
+Console.Write("Digite seu salario atual: R$");
+double salarioAtual = Convert.ToDouble(Console.ReadLine());
+
+//Solicitando em quantos % será o aumento
+Console.Write("Quanto será o aumento: (Exemplo '5' para 5% de aumento): ");
+int aumento = Convert.ToInt32(Console.ReadLine());
+
+//Calculando novo salario
+double novoSalario = salarioAtual+(salarioAtual * aumento / 100);
+
+//Imprimindo novo salario
+Console.WriteLine($"Parabens {nomeFuncionario}! Seu salario que antes era R${salarioAtual:N2}, com {aumento}% de aumento, seu novo salario será R${novoSalario:N2}!");
---
+++ ExerciciosC#/Exercicio14/Program.cs
@@ -0,0 +1,56 @@
+﻿// Desafio 14: Escreva um programa que converta uma temperatura digitada em ºC e converta para ºF.
+
+//Usando da biblioteca abaixo para tratamento de erro
+using System.Text.RegularExpressions;
+
+//Declarando variavel para a Temperatura em C
+int tempC;
+
+//Greetings!
+Console.WriteLine("Bem vindo ao conversor de temperaturas da professora Alexa!");
+
+//Loop
+while (true)
+{
+    //Tratamento de erro
+    while (true)
+    {
+        try
+        {
+            //Solicitando ao usuario uma temperatura em ºC
+            Console.Write("Insira uma temperatura em ºC: (Exemplo 32ºC): ");
+            tempC = Convert.ToInt32(Console.ReadLine());
+            break;
+
+        }
+        catch (FormatException ex) { Console.WriteLine("\nErro! Digite um valor numerico valido! (Exemplo 32)"); }
+    }
+
+    //Efetuando a conversão para ºF
+    int tempF = tempC * 9 / 5 + 32;
+
+    //Imprimindo resultado da conversão
+    Console.WriteLine($"{tempC}ºC é igual a {tempF}ºF.");
+
+    //Declarando variavel String para escolha do usuario
+    string c;
+
+    //Tratamento de erro
+    while (true)
+    {
+        Console.WriteLine("\nDeseja continuar? [S/N]");
+        c = Console.ReadLine().ToUpper();
+
+        if (!Regex.IsMatch(c, "[SNsn]")){
+            Console.WriteLine("\nErro! Digite somente S (para Sim) ou N (para Não)!");
+        }
+        else{break;}
+    }
+
+    if (c.Equals("N"))
+    {
+        Console.WriteLine("\nObrigado e volte sempre!");
+        break;
+    }
+
+}
