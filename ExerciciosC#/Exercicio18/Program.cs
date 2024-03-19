a8d0026b11b3200f889636a1e694433f1581e539
Julio Alves da Silva Jr <julioadsj@outlook.com>
2024-03-19 19:14:06 -0300

    Exercicio 18

--- ExerciciosC#/Exercicio18/Program.cs
+++ ExerciciosC#/Exercicio18/Program.cs
@@ -2,11 +2,62 @@
 
 Console.WriteLine("Bem vindo ao analizador de angulos, seno, cosseno e tangente da prof(a) Alexa!");
 
-Console.Write("Digite um valor para o angulo: ");
-double angulo = Convert.ToDouble(Console.ReadLine());
+//Declarando uma variavel para o Cateto Adjacente
+double ca;
 
-double seno = Math.Sin(angulo);
-double cosseno = Math.Cos(angulo);
-double tangente = Math.Tan(angulo);
+//Tratamento de erro
+while (true)
+{
+    try
+    {
+        //Solicitando um valor para o Cateto Adjacente
+        Console.Write("\nDigite um valor para o cateto adjacente: ");
+        ca = Convert.ToDouble(Console.ReadLine());
+        break;
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Erro! Por favor não digite valores alem de numeros!");
+    }
+}
 
-Console.WriteLine($"Seno: {seno}\nCosseno: {cosseno}\nTangente: {tangente}");
+//Declarando uma variavel para o Cateto Oposto
+double co;
+
+while (true)
+{
+    try
+    {
+        //Solicitando um valor para o Cateto Oposto
+        Console.Write("\nDigite um valor para o cateto oposto: ");
+        co = Convert.ToDouble(Console.ReadLine());
+        break;
+    }
+    catch (FormatException ex)
+    {
+        Console.WriteLine("Erro! Por favor não digite valores alem de numeros!");
+    }
+}
+
+//O valor de ca será elevado a 2º potencia (quadrado)
+double ca_quadrado = Math.Pow(ca, 2);
+
+//O valor de co será elevado a 2º potencia (quadrado)
+double co_quadrado = Math.Pow(co, 2);
+
+//Somando os valores acima
+double soma_CaCo = (ca_quadrado + co_quadrado);
+
+//Usando raiz quadrada para calcularmos a hipotenusa usando o resultado do calculo acima
+double hipo = Math.Sqrt(soma_CaCo);
+
+//Seno
+double seno = ca / hipo;
+
+//Cosseno
+double cosseno = co / hipo;
+
+//Tangente
+double tangente = co / ca;
+
+Console.WriteLine($"\nSeno: {seno}\nCosseno: {cosseno}\nTangente: {tangente}.");
